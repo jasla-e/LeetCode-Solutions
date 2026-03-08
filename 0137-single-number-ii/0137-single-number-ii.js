d11c8d1b26c5c02be1b413303c14fcf211e9562a
JASLA E <jaslaedakkodan@gmail.com>
2026-03-08 09:47:55 +0530

    Time: 0 ms (100%), Space: 58.2 MB (22.5%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+     let map = new Map();
+
+    for (let num of nums) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    for (let [key, value] of map) {
+        if (value === 1) {
+            return key;
+        }
+    }
+    
+};
