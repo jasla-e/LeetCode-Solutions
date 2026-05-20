794913327cab45249ab3895f155969bb9309cdfa
JASLA E <jaslaedakkodan@gmail.com>
2026-05-20 06:17:20 +0530

    Time: 3 ms (60.58%), Space: 65.2 MB (54.65%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0];
+    let profit = 0;
+
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] < min) {
+            min = prices[i];
+        } else {
+            profit = Math.max(profit, prices[i] - min);
+        }
+    }
+
+    return profit;
+};
