4031aa7bae2e048dac31f20bf0350af2c9fd5387
JASLA E <jaslaedakkodan@gmail.com>
2026-06-18 20:09:11 +0530

    Time: 0 ms (100%), Space: 55.2 MB (16.61%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1, m, nums2, n) {
+    nums1.splice(m, n);
+
+    for (let num of nums2) {
+        nums1.push(num);
+    }
+
+    nums1.sort((a, b) => a - b);
+}
+
+let nums1 = [1,2,3,0,0,0];
+let nums2 = [2,5,6];
+
+merge(nums1, 3, nums2, 3);
+
+console.log(nums1);
