5fb0e3ca836fc01fd4941e063ae1fb884fb86485
JASLA E <jaslaedakkodan@gmail.com>
2026-07-11 09:54:28 +0530

    Time: 1 ms (42.75%), Space: 55.4 MB (34.94%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let x = 0;
+
+    for (let operation of operations) {
+        if (operation.includes("++")) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+
+    return x;
+};
