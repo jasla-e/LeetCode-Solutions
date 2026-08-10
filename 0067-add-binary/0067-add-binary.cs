5552226966f69aeb718be984122c268f4ac560dd
JASLA E <jaslaedakkodan@gmail.com>
2026-08-10 09:47:59 +0530

    Time: 1 ms (87.59%), Space: 43.9 MB (17.77%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cs
@@ -0,0 +1,30 @@
+public class Solution
+{
+    public string AddBinary(string a, string b)
+    {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+
+        string result = "";
+
+        while (i >= 0 || j >= 0)
+        {
+            int sum = carry;
+
+            if (i >= 0)
+                sum += a[i--] - '0';
+
+            if (j >= 0)
+                sum += b[j--] - '0';
+
+            result = (sum % 2) + result;
+            carry = sum / 2;
+        }
+
+        if (carry == 1)
+            result = "1" + result;
+
+        return result;
+    }
+}
