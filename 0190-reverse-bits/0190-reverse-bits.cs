44b8cbfc86d210d2a7a031e7b2a2a215bba95b08
JASLA E <jaslaedakkodan@gmail.com>
2026-08-11 09:38:41 +0530

    Time: 24 ms (27.97%), Space: 28.8 MB (67.18%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int ReverseBits(int n) {
+        int result=0;
+
+        for (int i=0;i<32;i++){
+            result=result<<1;
+            result=result | n&1;
+            n=n>>1;
+        }
+        return result;
+    }
+}
