41ccf60b87202ea7e0c74b2b5370422f92ee87a9
JASLA E <jaslaedakkodan@gmail.com>
2026-08-29 09:50:09 +0530

    Time: 789 ms (87.59%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT Prices.product_id,ROUND(COALESCE(SUM(Prices.price*UnitsSold.units)/SUM(UnitsSold.units),0),2)As average_price
+FROM Prices 
+LEFT JOIN UnitsSold
+ON Prices.product_id=UnitsSold.product_id
+AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+GROUP BY Prices.product_id
