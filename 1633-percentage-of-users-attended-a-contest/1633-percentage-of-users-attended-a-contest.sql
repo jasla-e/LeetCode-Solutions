56eb56430a36f3bd7c83cf9e0461a2bbb16824b9
JASLA E <jaslaedakkodan@gmail.com>
2026-09-15 09:46:50 +0530

    Time: 992 ms (77.71%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,11 @@
+
+SELECT R.contest_id,
+       ROUND(
+           COUNT(DISTINCT R.user_id) * 100.0 /
+           (SELECT COUNT(*) FROM Users),
+           2
+       ) AS percentage
+FROM Register R
+GROUP BY R.contest_id
+ORDER BY percentage DESC, R.contest_id ASC;
+      
